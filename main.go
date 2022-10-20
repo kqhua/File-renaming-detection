07240db741b6e726e6ae8e397c737829774c33e7
kqhua <kyle.hua@datadoghq.com>
2022-10-20 04:26:58 -0400

    small demo to check file renaming

--- main.go
+++ main.go
@@ -5,62 +5,75 @@ import (
 	"os"
 	"os/exec"
 	re "regexp"
+	"strings"
 )
 
 func main() {
+	testRenameDiscovery()
+}
+
+// Test Functions
+
+// testRenameDiscovery renames a file and attempts to discover the changes. Reverts files to og state after
+func testRenameDiscovery() {
 	fmt.Println("Testing renaming file and detecting change")
-
-	//filename := "data.txt"
-	// makeFile(filename)
-	//newName := "pokemon.txt"
-	// renameFile(filename, newName)
-
-	//renameFile(filename, newName)
-
+	fmt.Println()
+	ogName := "first_name.txt"
+	newName := "second_name.txt"
+	renameFile(ogName, newName)
 	discoverRename()
-
+	renameFile(newName, ogName)
 }
 
-func makeFile(fileName string) {
-	f, err := os.Create(fileName)
+// Util Functions
 
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	defer f.Close()
-
-	_, err2 := f.WriteString("old falcon\n")
-
-	if err2 != nil {
-		fmt.Println(err2.Error())
-	}
-
-	fmt.Println("done")
-}
-
+// renameFile is a wrapper function to rename a file
 func renameFile(fileName string, newName string) {
-	fmt.Println("renaming " + fileName + " to " + newName)
+	fmt.Println("Renaming " + fileName + " to " + newName)
+	fmt.Println()
 	err := os.Rename(fileName, newName)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 }
 
-func lsa() {
-	cmd := exec.Command("ls", "-lah")
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Printf("cmd.Run() failed with %s\n", err)
-	}
-	fmt.Printf("combined out:\n%s\n", string(out))
+// discoverRename stages new changes in the repo with git add, searches for renamed line and prints it if found, then unstages changes.
+func discoverRename() {
+	fmt.Println("Running git add")
+	gitAdd()
+	out := gitStatus()
+	renamedLine := re.MustCompile(`(renamed.*?)(?:\r|\n|\r\n)`)
+	found := renamedLine.Find(out)
+	if found != nil {
 
-	if ok, _ := re.Match("pokemon.txt", out); ok {
-		err = nil
-		fmt.Println("found pokemon")
+		fmt.Printf("Discovered line: %q \n", found)
+		printRenameDiscovery(string(found))
+
+	}
+	fmt.Println("Running git reset")
+	fmt.Println()
+	gitReset()
+}
+
+func printRenameDiscovery(renameLine string) {
+	words := strings.Fields(renameLine)
+	ogFilename := words[1]
+	newFilename := words[3]
+
+	fmt.Println("Old name:", ogFilename)
+	fmt.Println("New name:", newFilename)
+}
+
+func discoverModified() {
+	out := gitStatus()
+	renamedLine := re.MustCompile(`(modified.*?)(?:\r|\n|\r\n)`)
+	found := renamedLine.FindAll(out, -1)
+	if found != nil {
+		fmt.Printf("%q\n", found)
 	}
 }
 
+// Git functions
 func gitStatus() []byte {
 	cmd := exec.Command("git", "status")
 	out, err := cmd.CombinedOutput()
@@ -72,38 +85,18 @@ func gitStatus() []byte {
 
 func gitAdd() {
 	cmd := exec.Command("git", "add", ".")
-	out, err := cmd.CombinedOutput()
+	_, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Printf("cmd.Run() failed with %s\n", err)
 	}
-	fmt.Printf("combined out:\n%s\n", string(out))
+	//fmt.Printf("combined out:\n%s\n", string(out))
 }
 
 func gitReset() {
 	cmd := exec.Command("git", "reset")
-	out, err := cmd.CombinedOutput()
+	_, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Printf("cmd.Run() failed with %s\n", err)
 	}
-	fmt.Printf("combined out:\n%s\n", string(out))
-}
-
-func discoverRename() {
-	gitAdd()
-	out := gitStatus()
-	renamedLine := re.MustCompile(`(renamed.*?)(?:\r|\n|\r\n)`)
-	found := renamedLine.Find(out)
-	if found != nil {
-		fmt.Printf("%q\n", found)
-	}
-	gitReset()
-}
-
-func discoverModified() {
-	out := gitStatus()
-	renamedLine := re.MustCompile(`(modified.*?)(?:\r|\n|\r\n)`)
-	found := renamedLine.FindAll(out, -1)
-	if found != nil {
-		fmt.Printf("%q\n", found)
-	}
+	//fmt.Printf("combined out:\n%s\n", string(out))
 }
